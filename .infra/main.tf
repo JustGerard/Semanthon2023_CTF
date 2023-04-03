f1c85fae55559657036a5b1e3439be2491d0d49e
Gerard Wiśniewski <gerwis1998@gmail.com>
2023-04-03 16:42:03 +0200

    Fix ctfd image

--- .infra/main.tf
+++ .infra/main.tf
@@ -13,7 +13,8 @@ provider "aws" {
 }
 
 module "ctfd" {
-  source     = "1nval1dctf/ctfd/aws"
-  aws_region = "eu-north-1"
-  ctfd_image = "public.ecr.aws/t1r2j1b6/my-ctfd:latest"
+  source                = "1nval1dctf/ctfd/aws"
+  https_certificate_arn = "arn:aws:acm:eu-north-1:995295403905:certificate/94e5d452-c6f6-4f46-8e83-ee0bcc21e7de"
+  aws_region            = "eu-north-1"
+  ctfd_image            = "public.ecr.aws/t1r2j1b6/ctf:latest"
 }
